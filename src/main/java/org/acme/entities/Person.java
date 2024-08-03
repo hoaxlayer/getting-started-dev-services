528b58a1d5b0eaf04cfe46bca2f49bd7a6912dbd
hoaxlayer <hector87@gmail.com>
2024-08-03 13:05:56 -0600

    removed lombok dep

--- src/main/java/org/acme/entities/Person.java
+++ src/main/java/org/acme/entities/Person.java
@@ -4,13 +4,11 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.*;
 import org.acme.Status;
 
 import java.time.LocalDate;
 
 @Entity
-@Getter @Setter
 public class Person{
 
     @Id @GeneratedValue
@@ -19,5 +17,37 @@ public class Person{
     public String name;
     public LocalDate birth;
     public Status status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirth() {
+        return birth;
+    }
+
+    public void setBirth(LocalDate birth) {
+        this.birth = birth;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 
 }
